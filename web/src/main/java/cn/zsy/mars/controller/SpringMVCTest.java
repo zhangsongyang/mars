58c4514e6df0a7535c71a058c42751b744426da1
328429080@qq.com <tcl581638ufo>
2016-03-15 01:36:37 +0800

    spring mvc 练习

--- web/src/main/java/cn/zsy/mars/controller/SpringMVCTest.java
+++ web/src/main/java/cn/zsy/mars/controller/SpringMVCTest.java
@@ -2,6 +2,7 @@ package cn.zsy.mars.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,6 +12,72 @@ public class SpringMVCTest {
 
     private static final String SUCCESS = "success";
 
+
+
+    /**
+     * Rest 风格的 URL. 以 CRUD 为例: 新增: /order POST 修改: /order/1 PUT update?id=1 获取:
+     * /order/1 GET get?id=1 删除: /order/1 DELETE delete?id=1
+     *
+     * 如何发送 PUT 请求和 DELETE 请求呢 ? 1. 需要配置 HiddenHttpMethodFilter 2. 需要发送 POST 请求
+     * 3. 需要在发送 POST 请求时携带一个 name="_method" 的隐藏域, 值为 DELETE 或 PUT
+     *
+     * 在 SpringMVC 的目标方法中如何得到 id 呢? 使用 @PathVariable 注解
+     *
+     */
+    @RequestMapping(value = "/testRest/{id}", method = RequestMethod.PUT)
+    public String testRestPut(@PathVariable Integer id) {
+        System.out.println("testRest Put: " + id);
+        return SUCCESS;
+    }
+
+    @RequestMapping(value = "/testRest/{id}", method = RequestMethod.DELETE)
+    public String testRestDelete(@PathVariable Integer id) {
+        System.out.println("testRest Delete: " + id);
+        return SUCCESS;
+    }
+
+    @RequestMapping(value = "/testRest", method = RequestMethod.POST)
+    public String testRest() {
+        System.out.println("testRest POST");
+        return SUCCESS;
+    }
+
+    @RequestMapping(value = "/testRest/{id}", method = RequestMethod.GET)
+    public String testRest(@PathVariable Integer id) {
+        System.out.println("testRest GET: " + id);
+        return SUCCESS;
+    }
+
+
+    /**
+     * @PathVariable 可以来映射 URL 中的占位符到目标方法的参数中.
+     * @param id
+     * @return
+     */
+    @RequestMapping("/testPathVariable/{id}")
+    public String testPathVariable(@PathVariable("id") Integer id) {
+        System.out.println("testPathVariable: " + id);
+        return SUCCESS;
+    }
+
+
+    @RequestMapping("/testAntPath/*/abc")
+    public String testAntPath() {
+        System.out.println("testAntPath");
+        return SUCCESS;
+    }
+
+    /**
+     * 了解: 可以使用 params 和 headers 来更加精确的映射请求. params 和 headers 支持简单的表达式.
+     *
+     * @return
+     */
+    @RequestMapping(value = "/testParamsAndHeaders", params = { "username",
+            "age!=10" }, headers = { "Accept-Language=zh-CN,zh;q=0.8" })
+    public String testParamsAndHeaders() {
+        System.out.println("testParamsAndHeaders");
+        return SUCCESS;
+    }
 
 
     /**
