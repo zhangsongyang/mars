537872727a451311b20487344dfbe03f822390ee
328429080@qq.com <tcl581638ufo>
2016-04-16 12:59:54 +0800

    spring + spring mvc + druid

--- web/src/main/java/cn/zsy/mars/entity/Person.java
+++ web/src/main/java/cn/zsy/mars/entity/Person.java
@@ -4,16 +4,54 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Person {
-	
-	
-	private String username;
-	
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	
-	public void hello(){
-		System.out.println("My name is " + username);
-	}
-	
+
+    private Integer id;
+
+    private String userName;
+
+    private Integer userAge;
+
+    private String userAddress;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Integer getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(Integer userAge) {
+        this.userAge = userAge;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", userAge=" + userAge +
+                ", userAddress='" + userAddress + '\'' +
+                '}';
+    }
 }
